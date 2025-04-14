ab7acd26a4c174e6485f6594cd0d4172b1765a78
Cagri TURK <haliscagri@gmail.com>
2025-04-14 07:55:51 +0300

    Add Service repository layer

---
+++ src/main/java/com/cagriturk/servicemanager/repository/ServiceRepository.java
@@ -0,0 +1,13 @@
+package com.cagriturk.servicemanager.repository;
+
+import com.cagriturk.servicemanager.collection.Service;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Repository interface for Service entities.
+ * Extends MongoRepository to provide CRUD operations for Service documents.
+ */
+@Repository
+public interface ServiceRepository extends MongoRepository<Service, String> {
+}
